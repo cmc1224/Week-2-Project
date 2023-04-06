8d9d84fe924ecd6358ca86e35bc463be11ad38d9
cmc1224 <cmc1224@nyu.edu>
2023-04-06 10:13:52 -0400

    karenhelp

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-var on = "img/lightswitch_on.png"
-var off =  "img/lightswitch_off.jpg"
+var lightswitch = document.getElementById('light').src
+var on =  "img/lightswitch_off.jpg"
+var off = "img/lightswitch_on.png"
 
 //changing image of lightswitch from on to off
 function changeImage() {
-    if (document.getElementById("switch").src == off) {
-        document.getElementById("switch").src = on ;
+    if (lightswitch == off) {
+        lightswitch = on
     } else {
-        document.getElementById("switch").src = off;
-    }
+        lightswitch = off;
+    } console.log("test")
 }
 
 //changing background color to white when on or black when off
 function chBackcolor(white) {
-    if (document.getElementById("switch").src == on) {
+    if (lightswitch == on) {
         document.body.style.background = white;
     }
     else { document.body.style.background = 'black'; }
