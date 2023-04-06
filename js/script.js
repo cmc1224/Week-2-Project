49a3ab437b5ba663fa67136422a6545c74019075
cmc1224 <cmc1224@nyu.edu>
2023-04-06 10:31:13 -0400

    variable_name_change

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 var lightswitch = document.getElementById("light").src
-var off =  "img/lightswitch_off.jpg"
-var on = "img/lightswitch_on.png"
+var offImage =  "img/lightswitch_off.jpg"
+var onImage = "img/lightswitch_on.png"
 
 //changing image of lightswitch from on to off
 function changeImage() {
-    if (document.getElementById("light").src.includes(off)) {
-        document.getElementById("light").src = on ;
+    if (document.getElementById("light").src.includes(offImage)) {
+        document.getElementById("light").src = onImage ;
     } else {
-        document.getElementById("light").src = off;
+        document.getElementById("light").src = offImage;
     }
 }
 
 //changing background color to white when on or black when off
 function chBackcolor(white) {
-    if (document.getElementById("light").src.includes(on)) {
+    if (document.getElementById("light").src.includes(onImage)) {
         document.body.style.background = white;
     }
     else { document.body.style.background = 'black'; }
