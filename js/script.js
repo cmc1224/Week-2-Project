e79099da55534a59d977f72a89678985ad7a8b1d
cmc1224 <cmc1224@nyu.edu>
2023-04-06 10:28:05 -0400

    changes_switch_and_background

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-var lightswitch = document.getElementById('light').src
+var lightswitch = document.getElementById("light").src
 var off =  "img/lightswitch_off.jpg"
 var on = "img/lightswitch_on.png"
 
 //changing image of lightswitch from on to off
 function changeImage() {
-    if (lightswitch == off) {
-        lightswitch = on
+    if (document.getElementById("light").src.includes(off)) {
+        document.getElementById("light").src = on ;
     } else {
-        lightswitch = off;
-    } console.log("test")
+        document.getElementById("light").src = off;
+    }
 }
 
 //changing background color to white when on or black when off
 function chBackcolor(white) {
-    if (lightswitch == on) {
+    if (document.getElementById("light").src.includes(on)) {
         document.body.style.background = white;
     }
     else { document.body.style.background = 'black'; }
